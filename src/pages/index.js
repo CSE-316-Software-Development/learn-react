ae8147443dca835cc4bbe23e1a7c3d9aabf07421
Prabhleen Bindra <prabhleenbindra@Prabhleens-MacBook-Air.local>
2023-10-12 21:07:18 -0400

    in class work Oct 12

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,24 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
-
+// import Profile from './components/profile.js'
+// import Gallery from "./components/gallery.js";
+// import Profile from './qcomps/profile_mistake.js'
+import Firstcomp from './qcomps/firstcomp.js'
+import Bios from './qcomps/bios.js';
+import Profile from './components/profile_props.js'
+import TodoList from "./qcomps/todos.js";
+import Gallery from './qcomps/gallery_props.js'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        {/*<Gallery />*/}
+        {/*<Profile />*/}
+        {/*<Firstcomp />*/}
+        {/*<Bios />*/}
+        {/*<TodoList />*/}
+        {/*<Profile />*/}
+        <Gallery />
     </div>
   )
 }
--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -1,55 +1,59 @@
+const baseUrl = 'https://i.imgur.com/';
+
+function Profile(props) {
+  return (
+      <section className="profile">
+        <h2>{props.name}</h2>
+        <img
+            className="avatar"
+            src={baseUrl + props.imageID + 's.jpg'}
+            alt={props.name}
+            width={70}
+            height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            {props.profession}
+          </li>
+          <li>
+            <b>Awards: {props.awards.length} </b>
+            {props.awards.map((award, index) => (
+                <span key={index}>{award}</span>
+            ))}
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {props.discovered}
+          </li>
+        </ul>
+      </section>
+  );
+}
+
 export default function Gallery() {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
+      <div>
+        <h1>Notable Scientists</h1>
+        <Profile
+            name="Maria Skłodowska-Curie"
+            imageID=  'szV5sdG'
+            profession="physicist and chemist"
+            awards={[
+              "Nobel Prize in Physics",
+              "Nobel Prize in Chemistry",
+              "Davy Medal",
+              "Matteucci Medal"
+            ]}
+            discovered="polonium (element)"
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
+        <Profile
+            name="Katsuko Saruhashi"
+            imageID='YfeOqp2'
+            profession="geochemist"
+            awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+            discovered="a method for measuring carbon dioxide in seawater"
         />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
-    </div>
+      </div>
   );
 }
