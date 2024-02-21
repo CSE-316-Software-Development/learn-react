4207368e46399fde17e5b59e008eb2f5e76e00d1
Ankur Haritosh <30547888+ankurharitosh@users.noreply.github.com>
2024-02-20 23:50:54 -0500

    Update firstcomp.js

--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,12 @@
-function MyComp() {}
+import React from 'react';
+
+function MyComp() {
+    return (
+        <div>
+            <h1>Hello from MyComp!</h1>
+            <p>This is a simple functional component.</p>
+        </div>
+    );
+}
+
+export default MyComp;
